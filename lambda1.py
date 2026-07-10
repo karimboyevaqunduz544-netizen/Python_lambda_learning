c6de772e207976a99ffb1bdbbf6fc225506015a7
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-07-10 17:11:56 +0500

    Create lambda1.py

---
+++ lambda1.py
@@ -0,0 +1,6 @@
+# Lambda funksiyasini o'zgaruvchiga biriktiramiz
+hisobla = lambda a: a * 2 if a % 2 == 0 else a
+
+# Funksiyani chaqiramiz va natijani chiqaramiz
+print(hisobla(8))  # Juft bo'lgani uchun 16 chiqadi
+print(hisobla(5))  # Toq bo'lgani uchun 5 chiqadi
