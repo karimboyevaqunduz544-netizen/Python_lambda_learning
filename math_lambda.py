6f17d73a7091d4424ec6bb442b1fa5523cb408e8
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-09-02 03:12:16 +0500

    Create math_lambda.py

---
+++ math_lambda.py
@@ -0,0 +1,10 @@
+
+#1! lambda(math)
+
+import math # math modulini chaqirib oldik
+uzunlik = lambda pi, r : 2*pi*r #lambda funksiyasini yaratdik, pi ,r argumentlariga 2*pi*r qiymat berdik
+print(uzunlik(math.pi,10))#62.83185307179586
+#2!
+
+product = lambda x ,y: x ** y
+print(product(3, 2)) #9
